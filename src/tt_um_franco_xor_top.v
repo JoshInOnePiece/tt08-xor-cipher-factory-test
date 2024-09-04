27f63908a735f2666f1247b07363ad6d2e1c5deb
JoshInOnePiece <jo634076@ucf.edu>
2024-09-04 13:52:03 -0400

    Changed pins to match tinyTapeout Board

--- src/tt_um_franco_xor_top.v
+++ src/tt_um_franco_xor_top.v
@@ -1,14 +1,19 @@
 module tt_um_franco_xor_top(
-    input iSerial_in,       // Serial DATA in
-    input iClk,             // ESP32 Input Clock.
-    input iRst,             // Active low reset.
-    input iEn,              // Enable Pin
-    input iLoad_key,        // Key load flag.
-    input iLoad_msg,        // Message load flag.
+    // input iClk,             // ESP32 Input Clock. clk pin
+    // input iRst,             // Active low reset. ui_in[0]
+    // input iEn,              // Enable Pin        ui_in[2]
+    // input iSerial_in,       // Serial DATA in    ui_in[1]
+    // input iLoad_key,        // Key load flag.    ui_in[3]
+    // input iLoad_msg,        // Message load flag.ui_in[4]
 
-    output oSerial_out,
-    output oSerial_start,
-    output oSerial_end
+    // output oSerial_out,  uo_out[0]
+    // output oSerial_start,uo_out[1]
+    // output oSerial_end   uo_out[2]
+
+    input clk,              //ESP32 Input Clock
+    input ui_in[4:0],
+    output uo_out[2:0]
+
 );
 
 // Key and message wires
@@ -26,30 +31,30 @@ wire encrypt_done;
 
 // Instantiate the key deserializer module
 deserializer #(.DATA_SIZE(32)) deserializer_key (
-    .iClk(iClk),
-    .iRst(iRst),
-    .iEn(iEn),
-    .iData_in(iSerial_in),
-    .iLoading(iLoad_key),
+    .iClk(clk),
+    .iRst(ui_in[0]),    //iRst
+    .iEn(ui_in[1]),      //iEn
+    .iData_in(ui_in[2]),  //iSerial_in
+    .iLoading(ui_in[3]),   //iLoad_key
     .oData(oKey),
     .oBit_counter(bit_counter_key)  // Correctly assign the key bit counter
 );
 
 // Instantiate the message deserializer module
 deserializer #(.DATA_SIZE(512)) deserializer_message (
-    .iClk(iClk),
-    .iRst(iRst),
-    .iEn(iEn),
-    .iData_in(iSerial_in),
-    .iLoading(iLoad_msg),
+    .iClk(clk),
+    .iRst(ui_in[0]),    //iRst
+    .iEn(ui_in[1]),      //iEn
+    .iData_in(ui_in[2]),
+    .iLoading(ui_in[4]),   //iLoad_msg
     .oData(oMessage),
     .oBit_counter(bit_counter_message)  // Correctly assign the message bit counter
 );
 
 // Instantiate the key assembler module
 key_assembler key_assembler_inst (
-    .iClk(iClk),
-    .iRst(iRst),
+    .iClk(clk),
+    .iRst(ui_in[0]),    //iRst
     .iKey(oKey),
     .iBit_counter_key(bit_counter_key),
     .oAssembled_key(oAssembled_key),
@@ -59,8 +64,8 @@ key_assembler key_assembler_inst (
 
 // Instantiate the XOR encryption module
 xor_encrypt #(.KEY_SIZE(32), .MSG_SIZE(512)) xor_encryptor (
-    .iClk(iClk),
-    .iRst(iRst),
+    .iClk(clk),
+    .iRst(ui_in[0]),    //iRst
     .iCan_encrypt(can_encrypt),  
     .iKey(oAssembled_key),       
     .iMessage(oMessage),         
@@ -72,13 +77,13 @@ xor_encrypt #(.KEY_SIZE(32), .MSG_SIZE(512)) xor_encryptor (
 
 // Instantiate the serializer module
 serialize #(.MSG_SIZE(512)) serialize_output (
-    .iClk(iClk),
-    .iRst(iRst),
+    .iClk(clk),
+    .iRst(ui_in[0]),    //iRst
     .iEncrypt_done(encrypt_done),
     .iCiphertext(oCiphertext),
-    .oSerial_out(oSerial_out),
-    .oSerial_start(oSerial_start),
-    .oSerial_end(oSerial_end)
+    .oSerial_out(uo_out[0]),
+    .oSerial_start(uo_out[1]),
+    .oSerial_end(uo_out[2])
 );
 
 endmodule
