6f30864f5f57f9e2c4d4a32791d470a3b6aca197
JoshInOnePiece <jo634076@ucf.edu>
2024-09-04 14:12:08 -0400

    Relabeled pins

--- src/tt_um_franco_xor_top.v
+++ src/tt_um_franco_xor_top.v
@@ -1,17 +1,19 @@
 module tt_um_franco_xor_top(
     // input iClk,             // ESP32 Input Clock. clk pin
-    // input iRst,             // Active low reset. ui_in[0]
-    // input iEn,              // Enable Pin        ui_in[2]
-    // input iSerial_in,       // Serial DATA in    ui_in[1]
-    // input iLoad_key,        // Key load flag.    ui_in[3]
-    // input iLoad_msg,        // Message load flag.ui_in[4]
+    // input iRst,             // Active low reset. rst_n
+    // input iEn,              // Enable Pin        ena
+    // input iSerial_in,       // Serial DATA in    ui_in[0]
+    // input iLoad_key,        // Key load flag.    ui_in[1]
+    // input iLoad_msg,        // Message load flag.ui_in[2]
 
     // output oSerial_out,  uo_out[0]
     // output oSerial_start,uo_out[1]
     // output oSerial_end   uo_out[2]
 
     input clk,              //ESP32 Input Clock
-    input  [4:0] ui_in,
+    input ena,
+    input rst_n,
+    input  [2:0] ui_in,
     output [2:0] uo_out
 
 );
@@ -26,16 +28,19 @@ wire [$clog2(32)  : 0] bit_counter_key;
 wire [$clog2(512) : 0] bit_counter_message;
 wire [$clog2(512) : 0] key_assemble_counter;
 
+wire iRst;
 wire can_encrypt;
 wire encrypt_done;
+
+assign iRst = ~rst_n;
 
 // Instantiate the key deserializer module
 deserializer #(.DATA_SIZE(32)) deserializer_key (
     .iClk(clk),
-    .iRst(ui_in[0]),    //iRst
-    .iEn(ui_in[1]),      //iEn
-    .iData_in(ui_in[2]),  //iSerial_in
-    .iLoading(ui_in[3]),   //iLoad_key
+    .iRst(iRst),    //iRst
+    .iEn(ena),      //iEn
+    .iData_in(ui_in[0]),  //iSerial_in
+    .iLoading(ui_in[1]),   //iLoad_key
     .oData(oKey),
     .oBit_counter(bit_counter_key)  // Correctly assign the key bit counter
 );
@@ -43,10 +48,10 @@ deserializer #(.DATA_SIZE(32)) deserializer_key (
 // Instantiate the message deserializer module
 deserializer #(.DATA_SIZE(512)) deserializer_message (
     .iClk(clk),
-    .iRst(ui_in[0]),    //iRst
-    .iEn(ui_in[1]),      //iEn
-    .iData_in(ui_in[2]),
-    .iLoading(ui_in[4]),   //iLoad_msg
+    .iRst(iRst),    //iRst
+    .iEn(ena),      //iEn
+    .iData_in(ui_in[0]), //serial_in
+    .iLoading(ui_in[2]),   //iLoad_msg
     .oData(oMessage),
     .oBit_counter(bit_counter_message)  // Correctly assign the message bit counter
 );
@@ -54,7 +59,7 @@ deserializer #(.DATA_SIZE(512)) deserializer_message (
 // Instantiate the key assembler module
 key_assembler key_assembler_inst (
     .iClk(clk),
-    .iRst(ui_in[0]),    //iRst
+    .iRst(iRst),    //iRst
     .iKey(oKey),
     .iBit_counter_key(bit_counter_key),
     .oAssembled_key(oAssembled_key),
@@ -65,7 +70,7 @@ key_assembler key_assembler_inst (
 // Instantiate the XOR encryption module
 xor_encrypt #(.KEY_SIZE(32), .MSG_SIZE(512)) xor_encryptor (
     .iClk(clk),
-    .iRst(ui_in[0]),    //iRst
+    .iRst(iRst),    //iRst
     .iCan_encrypt(can_encrypt),  
     .iKey(oAssembled_key),       
     .iMessage(oMessage),         
@@ -78,7 +83,7 @@ xor_encrypt #(.KEY_SIZE(32), .MSG_SIZE(512)) xor_encryptor (
 // Instantiate the serializer module
 serialize #(.MSG_SIZE(512)) serialize_output (
     .iClk(clk),
-    .iRst(ui_in[0]),    //iRst
+    .iRst(iRst),    //iRst
     .iEncrypt_done(encrypt_done),
     .iCiphertext(oCiphertext),
     .oSerial_out(uo_out[0]),
