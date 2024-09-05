ef102ffeeeee98e28254b4523b1a3784e5881060
Joshua Joseph <jo634076@ucf.edu>
2024-09-05 12:02:22 -0400

    Fixed syntax error in inequality

--- src/assemble_key.v
+++ src/assemble_key.v
@@ -16,7 +16,7 @@ module key_assembler (
         end else begin
             if (iBit_counter_key == 32) begin
                 // Assemble the key in 512-bit register by assigning each 32-bit segment
-                if (oKey_assemble_counter =< 511) begin
+                if (oKey_assemble_counter <= 511) begin
                     oAssembled_key[oKey_assemble_counter +: 32] <= iKey;  // Assign 32 bits at a time
                     oKey_assemble_counter <= oKey_assemble_counter + 32;
                 end
